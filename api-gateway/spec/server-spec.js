e115b8da6e4a27aad23d8d804cad2161d2a7451d
Francisco José Jordán Jiménez <fjjj0001@red.ujaen.es>
2023-04-20 12:52:00 +0200

    Añado expects api-gateway
    Añado los expects de las funciones POST, incluido el método addJugador, en el fichero server-spec.js del api-gateway

--- api-gateway/spec/server-spec.js
+++ api-gateway/spec/server-spec.js
@@ -46,7 +46,7 @@ describe('API Gateway: rutas estáticas', () => {
         .expect('Content-Type', /json/)
         .expect(function (res) {
           //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
-          assert(res.body.data.length === 10);
+          assert(Array.isArray(res.body.data));
           assert(res.body.data[0].data.hasOwnProperty('nombre'));
           assert(res.body.data[0].data.nombre === "Sergio");
 
@@ -68,6 +68,125 @@ describe('API Gateway: rutas estáticas', () => {
         })
         .end((error) => { error ? done.fail(error) : done(); }
         );
+    });
+
+    it('Devuelve Luis al recuperar los datos de la Persona con id 361430845704110285 mediante setCambios', (done) => {
+      const NOMBRE_TEST= 'Luis'
+      const jugador = {
+        id_jugador: '361430845704110285',
+        nombre_jugador: NOMBRE_TEST,
+        apellidos_jugador: 'Lozano Martínez',
+        apodo_jugador: 'El búfalo',
+        fecha_nacimiento_jugador: {
+          dia: '9',
+          mes: '11',
+          año: '1988'
+        },
+        dorsal_jugador: 9,
+        posicion_jugador: 'Ala',
+        equipos_jugados_jugador: ''
+      };
+      supertest(app)
+        .post('/plantilla/set-cambios')
+        .send(jugador)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "Server-spec , /setTodo res.body", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data.hasOwnProperty('nombre'));
+          assert(res.body.data.nombre == NOMBRE_TEST);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+
+    });
+
+    it('Cambiamos de nuevo el nombre a Sergio y comprobamos que se cambia correctamente', (done) => {
+      const NOMBRE_TEST= 'Sergio'
+      const jugador = {
+        id_jugador: '361430845704110285',
+        nombre_jugador: NOMBRE_TEST,
+        apellidos_jugador: 'Lozano Martínez',
+        apodo_jugador: 'El búfalo',
+        fecha_nacimiento_jugador: {
+          dia: '9',
+          mes: '11',
+          año: '1988'
+        },
+        dorsal_jugador: 9,
+        posicion_jugador: 'Ala',
+        equipos_jugados_jugador: ''
+      };
+      supertest(app)
+        .post('/plantilla/set-cambios')
+        .send(jugador)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "Server-spec , /setTodo res.body", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data.hasOwnProperty('nombre'));
+          assert(res.body.data.nombre == NOMBRE_TEST);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+    it('Se añade el jugador a la base de datos y los datos son correctos', (done) => {
+      const jugador = {
+        nombre: "Marcos",
+        apellidos: 'Dioney Schmidt',
+        apodo: 'Marcao',
+        fecha_nacimiento: {
+          dia: '9',
+          mes: '1',
+          año: '1987'
+        },
+        dorsal: 29,
+        posicion: 'Portero',
+        equipos_jugados: [
+          'Moitas',
+          'Guarapauva',
+          'Tubarao Futsal',
+          'Jimbee Cartagena',
+          'Jaén FS',
+          'Ribera Navarra FS'
+        ]
+      };
+      supertest(app)
+        .post('/plantilla/add-jugador')
+        .send(jugador)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "Server-spec , /add-jugador res.body", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data.hasOwnProperty('nombre'));
+          assert(res.body.data.nombre === jugador.nombre);
+
+          assert(res.body.data.hasOwnProperty('apellidos'));
+          assert(res.body.data.apellidos === jugador.apellidos);
+
+          assert(res.body.data.hasOwnProperty('apodo'));
+          assert(res.body.data.apodo === jugador.apodo);
+
+          assert(res.body.data.fecha_nacimiento.dia === jugador.fecha_nacimiento.dia);
+          assert(res.body.data.fecha_nacimiento.mes === jugador.fecha_nacimiento.mes);
+          assert(res.body.data.fecha_nacimiento.año === jugador.fecha_nacimiento.año);
+
+          assert(res.body.data.hasOwnProperty('dorsal'));
+          assert(res.body.data.dorsal === jugador.dorsal);
+
+          assert(res.body.data.hasOwnProperty('posicion'));
+          assert(res.body.data.posicion === jugador.posicion);
+
+          //console.log("Lo que obtengo del res: " + res.body.data.equipos_jugados)
+          //console.log("Los equipos del jugador: " + jugador.equipos_jugados)
+          assert(res.body.data.hasOwnProperty('equipos_jugados'));
+          assert(JSON.stringify(res.body.data.equipos_jugados) === JSON.stringify(jugador.equipos_jugados));
+
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+
     });
   })
 });
